904f9b90e7339de148b2253b3fa400865275bd87
vpsxkqktm <sd1593@gmail.com>
2022-08-08 21:29:31 +1200

    add NUnit test

---
+++ SectionThree/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SectionThree
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
